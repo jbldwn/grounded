f80425b828e67169bb74ed9ac3a9d9316a947f11
jbldwn <58793978+jbldwn@users.noreply.github.com>
2025-05-30 15:40:45 -0400

    updating notes on routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,15 +6,18 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-// Categories
+/* Categories */
 $routes->get('categories/selected/(:num)', 'Categories::selected/$1');
 $routes->get('/categories', 'Categories::index');
+//add new category
 $routes->get('/categories/new', 'Categories::new');
 $routes->post('/categories/add', 'Categories::add');
+//remove category
 $routes->get('/categories/remove', 'Categories::remove');
 $routes->post('/categories/delete', 'Categories::delete');
 
-// Questions
+/* Questions */
+//add question
 $routes->get('/questions/new', 'Questions::new');
 $routes->post('/questions/add', 'Questions::add');
 
