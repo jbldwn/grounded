83287ac75a0b8f50472cbeb0622f8b46eb9d148b
jbldwn <58793978+jbldwn@users.noreply.github.com>
2025-06-05 23:22:06 -0400

    cleaning up attempted code

--- app/Models/QuestionsModel.php
+++ app/Models/QuestionsModel.php
@@ -8,21 +8,5 @@ class QuestionsModel extends Model
     public $table = 'questions';
     protected $allowedFields = ['question', 'id'];
 
-    // public function getQuestionsWithCategories()
-    // {
-    //     return $this->select('questions.*, categoryquestionbank.question_id')
-    //                 ->join('categoryquestionbank', 'categoryquestionbank.question_id = questions.id')
-    //                 ->findAll();
-    // }
-    // public function addMultiCategoryQuestion($question_id, $category_id)
-    // {
-    //     $this->table('categoryquestionbank')->insert([
-    //         'question_id' => $question_id,
-    //         'category_id' => $category_id
-    //     ]);
-    //     return $this->select('questions.*, categoryquestionbank.question_id')
-    //                 ->join('categoryquestionbank', 'categoryquestionbank.question_id = questions.id')
-    //                 ->findAll();
-    // }
 }
 ?>
